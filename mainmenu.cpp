81497e2c8200553483a3c6526b70d1642f137557
KenCartagenas <2402256@ub.edu.ph>
2025-05-05 14:25:43 +0800

    Create mainmenu.cpp

---
+++ mainmenu.cpp
@@ -0,0 +1,19 @@
+#include "mainmenu.h"
+#include <iostream>
+#include "data.h"
+using namespace std;
+
+void runAndDetermine()
+{
+  
+}
+
+void teacherMenu()
+{
+  
+}
+
+void studentMenu()
+{
+  
+}
