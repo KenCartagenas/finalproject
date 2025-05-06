d06ca1e0af6c0e9810a6b8418c04080e8af50055
KenCartagenas <2402256@ub.edu.ph>
2025-05-06 15:29:13 +0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -4,6 +4,15 @@
 #include "data.h"
 using namespace std;
 
+void diplayMainMenuTemplate()
+{
+    system("cls")
+    cout << "    Welcome to " << logInCredential[loggedInID].username << endl;
+    cout << "-------------------------------------------------------" << endl;
+    cout << text;
+    cout << "-------------------------------------------------------" << endl;
+    cout << ">> ";
+}
 void mainMenu()
 {
   if(logInCredential[loggedInID].role == "teacher")
@@ -18,10 +27,26 @@ void mainMenu()
 
 void teacherMenu()
 {
-  
+  char option;
+  displayMainMenuTemplate(" a. Create New Course / Subject\nb. Open Existing Class\nc. Search Student\nd. Settings\ne. Log Out");
+  cin >> option;
+
+  switch(tolower(option))
+    {
+      case 'a': createCourse(); break;
+      case 'b': break;
+      case 'c': break;
+      case 'd': break;
+      case 'e': break;
+    }
 }
 
 void studentMenu()
+{
+  
+}
+
+void CreateCourse()
 {
   
 }
