030e2a0c3c3d7ce2a7b7a43e3007483bebf31591
KenCartagenas <2402256@ub.edu.ph>
2025-05-08 16:58:12 +0800

    Update data.h

--- data.h
+++ data.h
@@ -7,19 +7,11 @@ using namespace std;
 
 //g++ finalproject/main.cpp finalproject/account.cpp finalproject/data.cpp finalproject/ED.cpp finalproject/mainmenu.cpp -o main.exe 
 
-struct User
-{
-  string role;
-  int id;
-  int space;
-  string username;
-  string password;
-  string section;
-  int coursesEnrolled;
-};
-
 struct GradeRecord {
   int studentID;
+  vector<string> majorExamName;
+  vector<string> performanceTaskName;
+  vector<string> writtenTaskName;
   vector<float> majorExam;
   vector<float> performanceTask;
   vector<float> writtenTask;
@@ -35,8 +27,20 @@ struct Course
   float base;
   float majorExamPercentage;
   float performanceTaskPercentage;
-  float writtenTaskpercentage;
+  float writtenTaskPercentage;
   vector<GradeRecord> studentRecords;
+};
+
+struct User
+{
+  string role;
+  int id;
+  int space;
+  string username;
+  string password;
+  string section;
+  vector<int> coursesEnrolled;
+  vector<int> coursesHandled;
 };
 
 extern vector<Course> courses;
@@ -45,6 +49,8 @@ extern bool isLoggedIn;
 extern bool isRunning;
 extern int loggedInID;
 extern const int BASE_ID;
+extern int coursesCount;
+extern int userIndex;
 
 void loadData();
 void saveData();
