a0a413c2628ccedf2b75a11b1bc2b49f1da79d70
KenCartagenas <2402256@ub.edu.ph>
2025-05-08 16:58:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,13 @@ using namespace std;
 
 int main()
 {
-    int exit;
     //loadData();
     while(isRunning)
     {
         accountMenu();
         if(isLoggedIn == true)
         {
-            cout << "wow" << endl;
-            cin >> exit;
-            if (exit == 1)
-            {
-                break;
-            }
-            
-            //mainMenu();
+            mainMenu();
         }
         //saveData();
     }
