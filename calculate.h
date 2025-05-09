7fd6df0d83635bd84fb870181f05228ed107bec8
KenCartagenas <2402256@ub.edu.ph>
2025-05-09 16:05:37 +0800

    Update calculate.h

---
+++ calculate.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+
+float calculateGrade(int indexOfCourse);
+float calculateGPA();
+float calculatePerformanceTask(int indexOfCourse);
+float calculateWrittenTask(int indexOfCourse);
+float calculateMajorExam(int indexOfCourse);
+float calculateNeededScores();
+
+#endif
