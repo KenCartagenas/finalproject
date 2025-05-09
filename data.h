109807bf0ed4b04e49289fad07bfd43b5ace06cd
KenCartagenas <2402256@ub.edu.ph>
2025-05-09 16:06:13 +0800

    Update data.h

--- data.h
+++ data.h
@@ -6,15 +6,19 @@
 using namespace std;
 
 //g++ finalproject/main.cpp finalproject/account.cpp finalproject/data.cpp finalproject/ED.cpp finalproject/mainmenu.cpp -o main.exe 
-
+//g++ main.cpp account.cpp data.cpp ED.cpp mainmenu.cpp -o main.exe    
 struct GradeRecord {
   int studentID;
+  float finalGrade;
   vector<string> majorExamName;
   vector<string> performanceTaskName;
   vector<string> writtenTaskName;
   vector<float> majorExam;
   vector<float> performanceTask;
   vector<float> writtenTask;
+  vector<float> majorExamOver;
+  vector<float> performanceTaskOver;
+  vector<float> writtenTaskOver;
 };
 
 struct Course
@@ -36,9 +40,11 @@ struct User
   string role;
   int id;
   int space;
+  float GPA;
   string username;
   string password;
   string section;
+  vector<int> finalGrades;
   vector<int> coursesEnrolled;
   vector<int> coursesHandled;
 };
