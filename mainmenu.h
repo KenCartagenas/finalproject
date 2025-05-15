4adf9a44afa8fd5ba2f2a80428ee0c69d3fd3d5d
KenCartagenas <2402256@ub.edu.ph>
2025-05-15 15:56:50 +0800

    Update mainmenu.h

--- mainmenu.h
+++ mainmenu.h
@@ -1,6 +1,7 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 #include <string>
+#include "data.h"
 using namespace std;
 
 void displayMainMenuTemplate(string text);
@@ -16,10 +17,11 @@ void addScoreToStudent(int indexOfCourse);
 void addAdditionalScoreToStudent(int indexOfCourse);
 void editScore(int indexOfCourse);
 void generateReportCourse(int indexOfCourse);
-void generateReportGPA(int indexOfCourse);
+void generateReportGPA();
 void addOrRemove(int indexOfCourse);
 void customizeGradeSettings(int indexOfCourse);
 void searchStudentSummary();
 void editUserProfile();
+void openCourse(const User& student);
 
 #endif
